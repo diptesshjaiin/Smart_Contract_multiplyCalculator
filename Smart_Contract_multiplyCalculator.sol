ab690221d64922fd9ef194782ae5ede6d51b26eb
diptesshjaiin <diptesshjaiin@gmail.com>
2023-03-07 12:35:08 +0530

    Initial commit

---
+++ Smart_Contract_multiplyCalculator.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract learnFunctions {
+    function multiplyCalculator(uint a, uint b) public view returns (uint) {
+         uint a;
+      uint b;
+       uint result = a * b;
+       return result;
+    }
+}
